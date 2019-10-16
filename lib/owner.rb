50ecb36d6064aaece50c4b84de6f7f90cea83135
Aniece Thompson <aniecet@yahoo.com>
2019-10-16 21:43:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species, :cat
+  @@all =[]
+  
+  def initialize(name)
+    @name= name
+    @species
+    @@all << self
+  end
+  
+  def species
+    "human"
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    @@all= []
+  end
+  
+  def cats 
+  Cat.all.select do |cat|
+    
+    binding.pry
+    Cat.all.name == @name
+  end
+  end
 end
